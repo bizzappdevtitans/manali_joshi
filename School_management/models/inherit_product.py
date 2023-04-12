51fb34826be1b6ad6121d65b26cd5f9c469505aa
NIJ <NIJ@bizzappdev.com>
2023-04-12 12:32:52 +0530

    [ADD][T00308]School_management:Mail Attachments.

--- School_management/models/inherit_product.py
+++ School_management/models/inherit_product.py
@@ -1,12 +1,14 @@
 from odoo import fields, models
 
+
 class InheritProduct(models.Model):
     _inherit = "product.product"
 
     weight_ok = fields.Boolean(string="Weight Done")
-    purchase_order_ids = fields.One2many("purchase.order.line","product_id",limit=5)
+    purchase_order_ids = fields.One2many("purchase.order.line", "product_id", limit=5)
 
-    sale_order_ids = fields.One2many("sale.order.line","product_id",limit=5)
+    sale_order_ids = fields.One2many("sale.order.line", "product_id", limit=5)
+    manufractures_order_ids = fields.One2many("mrp.production", "product_id")
 
 
 class InheritProducttemplate(models.Model):
